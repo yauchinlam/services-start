aa50dddef131ddc3e4774e00000a6a897c77350d
Yauchin M. Lam <yauchinlam17@gmail.com>
2022-04-22 18:49:21 -0400

    Added new account update service

--- services-start/src/app/account/account.component.ts
+++ services-start/src/app/account/account.component.ts
@@ -1,23 +1,25 @@
+import { AccountsService } from './../accounts.service';
 import { LoggingService } from './../logging.service';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css'],
-  providers: [LoggingService]
+  providers: [LoggingService, AccountsService]
 })
 export class AccountComponent {
   @Input() account: {name: string, status: string};
   @Input() id: number;
-  @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
 
-  constructor(private logginService:LoggingService){
+  constructor(private logginService:LoggingService,
+              private accountsService: AccountsService){
 
   }
 
   onSetTo(status: string) {
-    this.statusChanged.emit({id: this.id, newStatus: status});
+    this.accountsService.updateStatus(this.id, status);
     this.logginService.logStatusChange(status);
   }
 }
